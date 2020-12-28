ca0d250682f5d19c0ab3fdf0eaea4aab1b088fde
luisCermeno <luis.cermeno.f@gmail.com>
2020-12-27 22:18:39 -0500

    form submission implemented

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path('<str:title>', views.entry, name="entry")
+    path('entries/<str:title>', views.entry, name="entry"),
+    path('add', views.add, name="add")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django import forms
+from django.urls import reverse
 from . import util
 
+class NewEntryForm(forms.Form):
+  form_title = forms.CharField(label = "Title")
+  form_content = forms.CharField(label = "Content")
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -13,3 +18,26 @@ def entry(request, title):
         "title": title.capitalize(),
         "content": util.decode(title)
     })
+
+def add(request):
+  if request.method == "POST":
+    # stores the data retrieved from the POST request into the object inputform
+    inputform = NewEntryForm(request.POST)
+    # if form is valid(server side validation)
+    if inputform.is_valid():
+      # takes the string in the attribute "task" and content from the object inputform 
+      # and stores it in a variables called title and content 
+      title = inputform.cleaned_data["form_title"]
+      content = inputform.cleaned_data["form_content"]
+      # write in file
+      util.save_entry(title,content)
+      return HttpResponseRedirect(reverse("encyclopedia:index"))
+    # in case form is not valid, send back the form 
+    else:
+      return render(request, "encyclopedia/add.html", {
+        "form": inputform
+      })
+
+  return render(request, "encyclopedia/add.html", {
+    "form": NewEntryForm()
+  })
